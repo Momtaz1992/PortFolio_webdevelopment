c21097cc80c9f3de1fe557215dcb30474e9dc318
Momtaz1992 <momtaz.mostafa.asmar@gmail.com>
2023-04-04 10:07:08 +0200

    my last touch on my portfolio

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -16,3 +16,18 @@ tabs.forEach((tab) => {
     tab.classList.add('skills__active');
   });
 });
+//show SIDEBAR
+const navMenu = document.getElementById('sidebar'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose =document.getElementById('nav-close')
+
+if(navToggle){
+       navToggle.addEventListener('click', () =>{
+              navMenu.classList.add('show-sidebar')
+       })
+}
+if(navClose){
+       navClose.addEventListener('click', () =>{
+              navMenu.classList.remove('show-sidebar')
+       })
+}
