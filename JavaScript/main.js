a3640c94debab666636f7e4c4ffdc99d197ad100
Momtaz Asmar <122457138+Momtaz1992@users.noreply.github.com>
2023-09-26 09:16:15 +0200

    a little touch

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,4 +1,4 @@
-// SKILLS TABS 
+// SKILLS TABS
 const tabs = document.querySelectorAll('[data-target]'),
       tabContent = document.querySelectorAll('[data-content]');
 
@@ -9,25 +9,34 @@ tabs.forEach((tab) => {
       tabContents.classList.remove('skills__active');
     });
     target.classList.add('skills__active');
-
     tabs.forEach((tab) => {
       tab.classList.remove('skills__active');
     });
     tab.classList.add('skills__active');
   });
 });
-//show SIDEBAR
+
+// Show SIDEBAR
 const navMenu = document.getElementById('sidebar'),
       navToggle = document.getElementById('nav-toggle'),
-      navClose =document.getElementById('nav-close')
+      navClose = document.getElementById('nav-close');
 
-if(navToggle){
-       navToggle.addEventListener('click', () =>{
-              navMenu.classList.add('show-sidebar')
-       })
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-sidebar');
+  });
 }
-if(navClose){
-       navClose.addEventListener('click', () =>{
-              navMenu.classList.remove('show-sidebar')
-       })
+
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-sidebar');
+  });
 }
+
+// Close Sidebar when a link is clicked
+const sidebarLinks = document.querySelectorAll('#sidebar a');
+sidebarLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    navMenu.classList.remove('show-sidebar');
+  });
+});
